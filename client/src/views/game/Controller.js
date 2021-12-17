2fbe08a21037534c03566d4bb8db5c143e23989e
that-one-arab <muhammed.aldulaimi98@gmail.com>
2021-12-17 19:31:39 +0300

    initialized neccassry game components

--- client/src/views/game/Controller.js
+++ client/src/views/game/Controller.js
@@ -6,6 +6,30 @@ function RoleAssignment() {
     );
 }
 
+function Day() {
+    return (
+        <div>
+            <h1>Day</h1>
+        </div>
+    );
+}
+
+function Night() {
+    return (
+        <div>
+            <h1>Night</h1>
+        </div>
+    );
+}
+
+function Results(params) {
+    return (
+        <div>
+            <h1>Results</h1>
+        </div>
+    );
+}
+
 export default function Controller() {
     return (
         <div>
