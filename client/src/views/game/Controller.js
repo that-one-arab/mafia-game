2b7c3b49b0f0051f77d78e8bd117aaf622e740aa
MilesTheEdgy <mohammad.nadom98@gmail.com>
2021-12-18 22:17:58 +0300

    working on slideshow implementation

--- client/src/views/game/Controller.js
+++ client/src/views/game/Controller.js
@@ -1,3 +1,6 @@
+import { useState, useEffect } from 'react';
+import { Svg } from '../../assets/svg';
+import './style.css';
 function RoleAssignment() {
     return (
         <div>
@@ -30,10 +33,74 @@ function Results(params) {
     );
 }
 
+function AutoSlider() {
+    const els = [
+        <Svg className={'svg slide-img'} svg='mafia' />,
+        <Svg className={'svg slide-img'} svg='doctor' />,
+        <Svg className={'svg slide-img'} svg='investigator' />,
+    ];
+
+    const [slideIndex, setSlideIndex] = useState(-1);
+
+    useEffect(() => {
+        setInterval(() => {
+            // console.log({ slideIndex });
+            if (slideIndex + 1 === els.length) setSlideIndex(0);
+            else setSlideIndex(slideIndex + 1);
+        }, 1000);
+    }, [slideIndex, els.length]);
+
+    return (
+        <div>
+            {els.map((el, i) => (
+                <div
+                    key={i}
+                    className={`${
+                        slideIndex === i ? 'mySlides-on' : 'mySlides-off'
+                    } fade`}
+                >
+                    <div className='numbertext'> {i + 1} / 3</div>
+                    {el}
+                    <div className='text'>Caption Text</div>
+                </div>
+            ))}
+            {/* <div className='slideshow-container'>
+                <div className='mySlides fade'>
+                    <div className='numbertext'>1 / 3</div>
+                    <Svg className={'svg'} svg='mafia' />
+                    <div className='text'>Caption Text</div>
+                </div>
+                <div className='mySlides fade'>
+                    <div className='numbertext'>1 / 3</div>
+                    <Svg className={'svg'} svg='mafia' />
+                    <div className='text'>Caption Text</div>
+                </div>
+                <div className='mySlides fade'>
+                    <div className='numbertext'>1 / 3</div>
+                    <Svg className={'svg'} svg='mafia' />
+                    <div className='text'>Caption Text</div>
+                </div>
+            </div> */}
+            {/* 
+            <div style={{ textAlign: 'center' }}>
+                <span class='dot'></span>
+                <span class='dot'></span>
+                <span class='dot'></span>
+            </div> */}
+
+            {/* <Svg className={'svg'} svg='mafia' />
+            <Svg className={'svg'} svg='villager' />
+            <Svg className={'svg'} svg='doctor' />
+            <Svg className={'svg'} svg='investigator' /> */}
+        </div>
+    );
+}
+
 export default function Controller() {
     return (
         <div>
             <RoleAssignment />
+            <AutoSlider />
         </div>
     );
 }
