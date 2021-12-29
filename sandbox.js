0f66680913ed280daa9ba758abe37df0e712fbf1
MilesTheEdgy <mohammad.nadom98@gmail.com>
2021-12-29 10:23:18 +0300

    Working on role and team distribute implementation

--- sandbox.js
+++ sandbox.js
@@ -1,11 +1,111 @@
+/**
+ * 
+LENGTHS:
+    3 Players:
+        1 Mafia
+        2 Villagers
+
+    4 Players
+        1 Mafia
+        3 Villagers
+
+    5 Players
+        1 Mafia
+        4 Villagers
+
+    6 Players
+        2 Mafia
+        4 Villagers
+
+    7 Players
+        2 Mafia
+        5 Villagers
+
+    8 Players
+        3 Mafia
+        5 Villagers
+
+    9 Players
+        3 Mafia
+        6 Villagers
+ */
+
+const roles = [
+    {
+        role: 'doctor',
+        team: 'village',
+    },
+    {
+        role: 'investigator',
+        team: 'village',
+    },
+    {
+        role: 'villager',
+        team: 'village',
+    },
+    {
+        role: 'mafioso',
+        team: 'mafia',
+    },
+    {
+        role: 'godfather',
+        team: 'mafia',
+    },
+];
+const teams = ['village', 'mafia'];
+
+let players = [
+    {
+        name: 'Sam',
+    },
+    {
+        name: 'Mike',
+    },
+    {
+        name: 'Alex',
+    },
+    {
+        name: 'Katy',
+    },
+    {
+        name: 'Romeo',
+    },
+];
+
 /** */
-const number = function (busStops) {
-    if (busStops.length === 1) return busStops[0][0] + busStops[0][1];
-    return busStops.reduce((prevVal, currentVal) => {
-        if (Array.isArray(prevVal))
-            return prevVal[0] - prevVal[1] + (currentVal[0] - currentVal[1]);
-        return prevVal + (currentVal[0] - currentVal[1]);
-    });
+function shuffle(array) {
+    let currentIndex = array.length;
+    let randomIndex;
+
+    // While there remain elements to shuffle...
+    while (currentIndex !== 0) {
+        // Pick a remaining element...
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex--;
+
+        // And swap it with the current element.
+        [array[currentIndex], array[randomIndex]] = [
+            array[randomIndex],
+            array[currentIndex],
+        ];
+    }
+
+    return array;
+}
+
+players = shuffle(players);
+
+const assignTeams = (players) => {
+    let mafTeam = [];
+    let villTeam = [];
+
+    if (players.length <= 5) {
+        mafTeam.push(players[0]);
+        villTeam = players.slice(1);
+    } else {
+    }
+
+    return { mafTeam, villTeam };
 };
 
-console.log(number([[0, 0]]));
+console.log(assignTeams(players));
