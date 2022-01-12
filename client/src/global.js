d534184b9da8f0a87f394df445b50549c679790e
that-one-arab <91668821+that-one-arab@users.noreply.github.com>
2022-01-12 21:51:36 +0300

    Update global.js

--- client/src/global.js
+++ client/src/global.js
@@ -1,2 +1,1 @@
-// export const WEBSOCKET_ENDPOINT = 'http://localhost:8080';
-export const WEBSOCKET_ENDPOINT = 'https://projects.b2b-iys.muhammed-aldulaimi.com';
+export const WEBSOCKET_ENDPOINT = 'http://localhost:8080';
